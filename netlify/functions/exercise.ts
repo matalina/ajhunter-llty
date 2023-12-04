53055d39200650bf507c4b0ce764850a77fc39fd
Alicia Wilkerson <matalina@gmail.com>
2023-12-04 05:50:33 -0600

    example text

--- netlify/functions/exercise.ts
+++ netlify/functions/exercise.ts
@@ -6,7 +6,7 @@ const handler: Handler = async (event, context) => {
   const prompt = await getRandomPrompt() as any;
   const post = {
     title: `Writing Exercise (${(new Date()).toDateString()})`,
-    content: `<p>Today's writing exercise is: "${prompt.prompt}".</p>${(prompt?.example) ? "<p>" + prompt.example + "</p>": ''}<p><em>If you use a WordPress, pingbacks should automatically track back to this page if you put in a link. If you use another blogging system, post your link in the comments so others can follow your work. Spam will be deleted.</em></p>`,
+    content: `<p>Today's writing exercise is: "${prompt.prompt}".</p>${(prompt?.example) ? "<p><strong>Example:</strong> " + prompt.example + "</p>": ''}<p><em>If you use a WordPress, pingbacks should automatically track back to this page if you put in a link. If you use another blogging system, post your link in the comments so others can follow your work. Spam will be deleted.</em></p>`,
     date: (new Date()).toISOString(),
     status: 'publish',
     tags: ['Daily Prompt', 'postaday', 'Exercise of the Day', 'Write Every Day', 'Daily Post'],
